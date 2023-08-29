1b2f678a75d84c2859009d17e169d122ae70deb4
Dan McKeown <danielmckeown555@hotmail.co.uk>
2023-08-29 13:46:26 +0100

    attempting code fix

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -92,7 +92,7 @@ suite('Functional Tests with Zombie.js', function () {
           browser.assert.success;
           browser.assert.text('span#name', 'Cristoforo');
           browser.assert.text('span#surname', 'Colombo');
-          browser.assert.elements('span#dates', 1);
+          browser.assert.element('span#dates', 1);
           done();
         })
       })
@@ -104,7 +104,7 @@ suite('Functional Tests with Zombie.js', function () {
           browser.assert.success;
           browser.assert.text('span#name', 'Amerigo');
           browser.assert.text('span#surname', 'Vespucci');
-          browser.assert.elements('span#dates', 1);
+          browser.assert.element('span#dates', 1);
           done();
         })
       })
